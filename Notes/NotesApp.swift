7cb74757647f0498171bd4ac53fd0d62401cde4e
Jose Valentin Abundo Hernandez <valentin.abundo@bambu-mobile.com>
2025-02-19 15:07:19 -0600

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Developer on 19/02/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
